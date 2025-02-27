7201712c3716001a6b63824c64b1f89efdd27476
deniskhan22bd <odinstrelok02@gmail.com>
2025-02-28 00:34:06 +0500

    changes

--- miniproject/products/views.py
+++ miniproject/products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from users.permissions import IsAdmin
 from .models import Product, Category, Tag
 from .serializers import ProductSerializer, CategorySerializer, TagSerializer
 from rest_framework.views import APIView
@@ -8,17 +9,17 @@ from rest_framework.views import APIView
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdmin]
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdmin]
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdmin]
     def perform_create(self, serializer):
         serializer.save(seller=self.request.user)
         
